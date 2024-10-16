e128dc09bddd904c08424cc3d4ff119fafcdb2dd
Carlos Fernando Muñoz Ramos <munozramoscarlos@gmail.com>
2024-10-16 18:24:01 -0300

    HU1 1.0

--- app.py
+++ app.py
@@ -4,25 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("###### APP DEL CLIMA ######")
-
-city = input("+ Ingrese la ciudad de la que desee consultar el clima: ")
-
 APPID = os.getenv('APIKEY')
+LANG = os.getenv('IDIOMA')
 
-lang = 'es'
+unit = "metric" 
 
-medida = 'metric'
+try:
+    print("###### APP DEL CLIMA ######")
+    city = input("+ Ingrese la ciudad de la que desee consultar el clima: ")
+    city = city.lower()
 
-r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APPID}&lang={lang}&units={medida}')
-diccionarioClima = r.json()
-
-print(diccionarioClima)
-
-print(diccionarioClima['weather'][0]['main'])
-
-print(diccionarioClima['weather'][0]['description'])
-                                                               
-    
-
-print("Algo fallo (XoX)")
+    r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APPID}&lang={LANG}&units={unit}')
+    weather_dictionary = r.json()
+    print(f"""
+############################################################
++ En {city.capitalize()}:
+    ■ TEMPERATURA ACTUAL: {weather_dictionary['main']['temp']}°
+    ■ DESCRIPCION: {(weather_dictionary['weather'][0]['description'].capitalize())}
+    ■ TEMPERATURA MÍNIMA: {(weather_dictionary['main']['temp_min'])}° 
+    ■ TEMPERATURA MÁXIMA: {(weather_dictionary['main']['temp_max'])}°
+    ■ PORCENTAJE DE HUMEDAD: {weather_dictionary['main']['humidity']}% 
+############################################################ 
+""")
+except: 
+    print("# Error inesperado. Reinicie para intentar nuevamente")
